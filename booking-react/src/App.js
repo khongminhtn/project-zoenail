f5f089aec74d519eaef1e403f5db8876c6415eb0
khongminhtn <tuyenminhkhong@live.co.uk>
2020-11-19 19:51:34 +0000

    added styles to buttons and worked on Service component

--- booking-react/src/App.js
+++ booking-react/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom'
 import { Services } from './components/Services'
 import { Details } from './components/Details'
 
 class App extends React.Component{
     render() {
         return (
-            <Services />
+            <div>
+                <Services />
+                <Details />
+            </div>
         )
     }
 }
--- booking-react/src/components/Services.jsx
+++ booking-react/src/components/Services.jsx
@@ -1,18 +1,75 @@
 import React from 'react';
 
+// Deals with small extras of service "shellac" and "long/short"
 function ServiceDetails(props) {
     return(
         <div>{this.props.details}</div>
     )
 }
 
-function Service(props) {
-    return (
-        <div>{props.name}</div>
-    )
+
+// Deals with specific service
+class Service extends React.Component {
+    constructor(props) {
+        super(props)
+        this.handleClick=this.handleClick.bind(this) 
+        this.state= {
+            extendDiv: false
+        }
+    }
+
+    handleClick(e) {
+        e.preventDefault();
+        this.state.extendDiv ? this.setState({extendDiv: false}): this.setState({extendDiv: true})
+    }
+
+    render() {
+        return (
+            <div className="Service" onClick={this.handleClick}>
+                {this.props.name}
+                {
+                    this.props.name === "Full Set" 
+                    ? this.state.extendDiv 
+                        ? <ServiceDetails name="Full Set"/>
+                        : null
+
+                    : this.props.name === "Infill" 
+                    ? this.state.extendDiv 
+                        ? <ServiceDetails name="Infill"/>
+                        : null
+
+                    : this.props.name === "Pedicure and Manicure" 
+                    ? this.state.extendDiv 
+                        ? <ServiceDetails name="Pedicure and Manicure"/>
+                        : null
+
+                    : this.props.name === "Take Off" 
+                    ? this.state.extendDiv 
+                        ? <ServiceDetails name="Take Off"/>
+                        : null
+
+                    : this.props.name === "Other Services" 
+                    ? this.state.extendDiv 
+                        ? <ServiceDetails name="Other Services"/>
+                        : null
+                    : null
+                }
+            </div>
+        )
+    }
 }
 
+
+// Houses all services
 class Services extends React.Component{
+    constructor() {
+        super()
+        this.states = {
+            selected: ""
+        }
+    }
+
+
     render() {
         return (
             <div>
